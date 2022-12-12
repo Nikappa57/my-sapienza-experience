f2cbeb95a846d5447c73ad77cdaf34ed8f3eea30
Nikappa57 <lorenz.gaudino@gmail.com>
2022-12-13 00:55:14 +0100

    empty line fix
    now should be a 10/10 score

--- Introduzione alla programmazione/LabPython10/A_Ex2.py
+++ Introduzione alla programmazione/LabPython10/A_Ex2.py
@@ -1,7 +1,7 @@
 def A_Ex2(file):
     with open(file, encoding="UTF-8") as f:
         file = f.read()
-    lines = [s.split(' ') for s in file.split('\n') if s]
+    lines = [s.split(' ') for s in file.split('\n')]
     words = file.split()
 
     return {w: {i + 1 for i, l in enumerate(lines) if w in l} for w in set(words)}
