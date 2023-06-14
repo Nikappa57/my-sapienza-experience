c8db65944ae74d5b9ef609b3e41ff0543359645d
Nikappa57 <lorenz.gaudino@gmail.com>
2023-06-14 16:51:27 +0200

    [Es 8]
    Update

--- Complementi di programmazione/Es 8/es3.c
+++ Complementi di programmazione/Es 8/es3.c
@@ -1,17 +1,5 @@
 #include "es8.h"
 
-void copy_helper(IteratoreInsieme it, Insieme *s)
-{
-	T val;
-
-	if (hasNext(it))
-	{
-		val = next(it);
-		copy_helper(it, s);
-	}
-	*(s) = inserisci(*(s), val);
-}
-
 Insieme copy(Insieme s)
 {
 	IteratoreInsieme	it;
