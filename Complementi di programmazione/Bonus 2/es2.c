cbe2fa91379073dc2217bed5a2d04b1c6fd79d59
Nikappa57 <lorenz.gaudino@gmail.com>
2023-06-14 16:43:25 +0200

    [Bonus 2]
    Update

--- Complementi di programmazione/Bonus 2/es2.c
+++ Complementi di programmazione/Bonus 2/es2.c
@@ -1,18 +1,25 @@
 
 #include "aux.h"
 
-void sostituisci_numeri(TipoSCL *list, const char *s)
+bool is_nbr(char c)
 {
-	if ((*list == NULL) || (*s == '\0'))
+	if (c >= '0' && c <= '9')
+      return (true);
+  	return (false);
+}
+
+void sostituisci_numeri(TipoSCL *list, const char *s) {
+  	TipoSCL tmp;
+
+	if (!list || !(*list) || !(*s))
       return ;
-  	if ((*list)->info >= '0' && (*list)->info <= '9')
+  	if (is_nbr((*list)->info))
     {
-    	TipoSCL tmp = (TipoSCL) malloc(sizeof(TipoNodoSCL));
-      	tmp->next = (*list)->next;
-      	tmp->info = *s;
+    	tmp = (TipoSCL) malloc(sizeof(TipoNodoSCL));
+      	tmp->info = *s++;
+        tmp->next = (*list)->next;
+      	free(*list);
       	*list = tmp;
-      	sostituisci_numeri(&((*list)->next), s + 1);
     }
-  	else
-      sostituisci_numeri(&((*list)->next), s);
+  sostituisci_numeri(&((*list)->next), s);
 }
